cb823df60199c720a9cd1631da899d03c3d326c3
Sofiia <sopfiasheveleva@gmail.com>
2026-04-27 19:59:00 +0000

    exercise style - index, show

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,13 +1,16 @@
 class Exercise < ApplicationRecord
   DURATIONS = [15, 30, 45, 60, 90, 120]
   TYPES = [
-    "yoga",
-    "pilates",
-    "strength"
+    "Yoga",
+"Pilates",
+"Stretching",
+"Strength exercises",
+"Cardio"
   ]
 
   validates :duration, inclusion: { in: DURATIONS }
   validates :exercise_type, inclusion: { in: TYPES }
+  validates :duration, numericality: { greater_than: 0 }
 
   def start_time
     self.date
